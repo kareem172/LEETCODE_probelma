0bd764c8fe54d3d45a9d1d81b55c5e2d5d65b28b
Kareem Khalaf <105270419+kareem172@users.noreply.github.com>
2022-12-15 16:04:16 +0200

    Update Two Sum.py

--- 1. Two Sum/Two Sum.py
+++ 1. Two Sum/Two Sum.py
@@ -1,1 +1,8 @@
 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashMap = {nums[i]:i for i in range(len(nums))}
+        for i in range(len(nums)):
+            if target-nums[i] in hashMap and hashMap[target-nums[i]] != i:
+                return i, hashMap[target-nums[i]]
+                    
