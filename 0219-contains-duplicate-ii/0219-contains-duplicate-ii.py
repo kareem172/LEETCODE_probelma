a1f8510a6d5d3ebd6961cb5e7fe0f10d770d330f
Kareem Khalaf <105270419+kareem172@users.noreply.github.com>
2022-12-19 19:39:03 +0200

    Time: 1708 ms (23.87%), Space: 27.2 MB (55.50%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -3,15 +3,12 @@ class Solution:
         map = {}
         for i in range(len(nums)):
             if not nums[i] in map:
-                map[nums[i]] = [i]
+                map[nums[i]] = i
             else:
-                map[nums[i]].append(i)
+                if abs(map[nums[i]] - i) <= k:
+                    return True
+                else:
+                    map[nums[i]] = i
         
-        for key in map:
-            if len(map[key]) > 1:
-                for i in range(len(map[key])-1):
-                    if abs(map[key][i] - map[key][i + 1]) <= k:
-                        return True
-                    
         return False
         
